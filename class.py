07e32c6287bf91afc5b4913f69f2b4ebd7b78f0d
shishir8890 <skshishirr@gmail.com>
2023-08-13 12:53:39 +0600

    assessment-1

---
+++ class.py
@@ -0,0 +1,36 @@
+#Asserment
+'''
+longest option example
+and
+shortest option example
+'''
+print('Dhaka Medical college')   #Dhaka longest option example
+print('Dhaka post')  #Dhaka shortest option example
+
+print('University of Monitoba')  #University longest option example
+print('University of Dhaka')  #University shortest option example
+
+print('Baby calm Down lyrics')  #Baby longest option example
+print('Babylon')  #Baby shortest option example
+
+print('Cricket World Cup 2023')  #Cricket longest option example
+print('Cricket live')  #Cricket shortest option example
+
+print('Bombay Sweets & co.ltd')  #Bombay longest option example
+print('Bombay Cat')  #Bombay shortest option example
+
+print('Football league rankings')   #Football longest option example
+print('Football live')   #Football shortest option example
+
+print('Paper Background')  #Paper longest option example
+print('Paper Size')  #Paper shortest option example
+
+print('Knife Price in BD')  #Knife longest option example
+print('Knife Set')  #Knife shortest option example
+
+print('Saturday Weather Dhaka')  #Saturday longest option example
+print('Saturday Night')  #Saturday shortest option example
+
+print('Hello Meaning in Bengali')  #Hello longest option example
+print('Hello')   #Hello shortest option example
+
